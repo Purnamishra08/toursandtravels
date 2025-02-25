5b006d0d4049193482fabbeb79f517779a8dd6e8
amitkumarbarik97 <amitkumarbarik97@gmail.com>
2025-02-25 23:20:51 +0530

    page stracture design enhancement

--- resources/views/admin/managevehicles/manageVehiclePrice.blade.php
+++ resources/views/admin/managevehicles/manageVehiclePrice.blade.php
@@ -20,7 +20,7 @@
                     <div class="inner-layout">
                         <div class="container-fluid px-4 pt-3">
                         <nav class="tab-menu">
-        							<a href="{{ route('admin.managevehicles.addVehiclePrice') }}" class="tab-menu__item ">
+        							<a href="{{ route('admin.manageVehicleprice.addVehiclePrice') }}" class="tab-menu__item ">
         								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
         									<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
         								  </svg>
