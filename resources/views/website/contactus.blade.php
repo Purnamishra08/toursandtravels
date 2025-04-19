ef746191686a8af5040ebbaa8add0d1bbf352273
Purnamishra08 <mishrapurna0705@gmail.com>
2025-04-20 01:33:12 +0530

    contact us enhancements

--- resources/views/website/contactus.blade.php
+++ resources/views/website/contactus.blade.php
@@ -6,11 +6,11 @@
         <h1 class="page-name">Contact Us</h1>
         <ul class="breadcrumb-list">
             <li class="breadcrumb-item">
-                <a href="{{route('website.home')  }}" class="breadcrumb-link"><i class="bi bi-house"></i></a>
+                <a href="{{route('website.home')}}" class="breadcrumb-link"><i class="bi bi-house"></i></a>
             </li>
            
             <li class="breadcrumb-item">
-                <a href="#" class="breadcrumb-link active">Contact Us</a>
+                <a href="{{ route('website.contactus')}}" class="breadcrumb-link active">Contact Us</a>
             </li>
         </ul>
     </div>
