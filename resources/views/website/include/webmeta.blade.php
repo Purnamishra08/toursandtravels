2c7f8157bd1fd92dac0e0055b3a8899ce0409341
amitkumarbarik97 <amitkumarbarik97@gmail.com>
2025-06-23 22:25:36 +0530

    font link

--- resources/views/website/include/webmeta.blade.php
+++ resources/views/website/include/webmeta.blade.php
@@ -9,7 +9,10 @@
     <meta name="robots" content="index, follow">
     <link rel="canonical" href="{{ url()->current() }}">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/fav-icon.webp') }}">
-
+    <!-- Google font -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <!-- Preconnect: External sources -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
